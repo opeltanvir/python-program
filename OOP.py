96fc2665104b201cc5b26e2e13a70fe85888acf4
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2022-06-24 22:06:26 +0600

    oop

--- OOP.py
+++ OOP.py
@@ -2,11 +2,16 @@ def method_name(a,b):
     print("A methood")
 
 class person:
-    def __init__(self,person_name):
+    def __init__(self,person_name,date_of_birtth,ht):
        self.name = person_name
+       self.date_of_birth= date_of_birtth
+       self.hight=ht
 
     def get_name(self):
           return self.name
+
+    def get_summary(self):
+        return f"name:{self.name}, DoB:{self.date_of_birth},Hight:{self.hight}"
 
 method_name(10,20)
 a_person = person("apple")
