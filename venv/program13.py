6aeb2313595153524177f582d226fc3d5d7c6b3c
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2022-06-24 12:09:41 +0600

    new

---
+++ venv/program13.py
@@ -0,0 +1,9 @@
+grocery = ['rice','water','tomato','ginger']
+
+for item  in grocery:
+    if item =='water':
+        continue
+     print(item)
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
