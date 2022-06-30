e97ebe6e852d0b7c6a57970f972d0d6dac63f8cd
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2022-06-30 22:57:52 +0600

    ds

---
+++ Ds2.py
@@ -0,0 +1,18 @@
+from ds import Doublelinkedlist
+
+class stack:
+    def __init__(self):
+        self.__list = Doublelinkedlist()
+
+    def push(self):
+        pass
+
+    def pop(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def peek(self):
+        pass
+
