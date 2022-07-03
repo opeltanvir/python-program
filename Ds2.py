78e5776221f614c64872e93c6f688f93f61fcfbf
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2022-07-03 23:38:14 +0600

    ds

--- Ds2.py
+++ Ds2.py
@@ -17,6 +17,20 @@ class stack:
 
     def peek(self):
         return self.__list.back()
+    def __str__(self):
+        return self.__list.size
+
+class Queue:
+    def __init__(self):
+        self.__list= Doublelinkedlist()
+
+    def enqueue(self,val):
+        self.__list.add(val)
+
+    def deque(self):
+
+
+
 
 
 
